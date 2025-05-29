4af51bbda8f44419c6d44ab7878e02570f062028
Ruturaj <ruturaj.pandharkar23@pccoepune.org>
2025-05-29 14:48:08 +0530

    Add dotenv support and hide credentials

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
+require('dotenv').config();
 
 const feedbackRoutes = require('./routes/feedbackRoutes');
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
-mongoose.connect('mongodb://localhost:27017/mydb')
+const uri = process.env.MONGODB_URI;
+mongoose.connect(uri)
   .then(() => console.log('MongoDB connected successfully'))
   .catch(err => console.error('MongoDB connection error:', err));
 
